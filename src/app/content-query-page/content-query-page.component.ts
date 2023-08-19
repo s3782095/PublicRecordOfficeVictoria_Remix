e6582d1e68a28da113c9b6fd918543316893555a
Mitchell Gust <mitchellgust@hotmail.com>
2023-08-19 23:54:22 +1000

    Search by Enter Button and Fix Map/Plan/Drawing Search Query

--- src/app/content-query-page/content-query-page.component.ts
+++ src/app/content-query-page/content-query-page.component.ts
@@ -13,10 +13,9 @@ export class ContentQueryPageComponent {
   type_of_record: string = '';
   results: any = [];
   API_BASE_URL: string = 'https://api.prov.vic.gov.au/search/';
-  query_url: string = '';
   imageCache: { [key: string]: string } = {};
 
-  RECORD_TYPES: string[] = ["File", "Document", "Map, Plan, Or Drawing", "Photograph or Image", "Volume", "Card",];
+  RECORD_TYPES: string[] = ["File", "Document", "Map, Plan, or Drawing", "Photograph or Image", "Volume", "Card",];
   chosen_record_types: string[] = ["Photograph or Image"];
 
   constructor(private http: HttpClient) {
@@ -24,29 +23,30 @@ export class ContentQueryPageComponent {
 
   constructQueryUrl(): string {
     const queryParams = `query?wt=json&q=(text%3A%22${encodeURIComponent(this.search_request)}%22)%20AND%20((record_form%3A%22${encodeURIComponent(this.selectOneRandomRecordTypes())}%22))%20AND%20(iiif-manifest%3A(*))`;
-    return this.query_url = this.API_BASE_URL + queryParams;
+    return this.API_BASE_URL + queryParams;
   }
 
   selectOneRandomRecordTypes(): string {
     return this.type_of_record = this.chosen_record_types[Math.floor(Math.random() * this.chosen_record_types.length)];
   }
 
-  SearchQuery() {
-    const queryUrl = this.constructQueryUrl();
+  SearchQuery(): Promise<any> {
+    let queryUrl = this.constructQueryUrl();
 
-    this.http.get<any>(queryUrl).subscribe({
-      next: (data) => {
-        if (data['response']['docs'][0] != undefined) {
-          this.results = data['response']['docs'][0];
-          console.log(this.results)
-        } else {
-          this.results = undefined;
-          console.log(this.results)
+    return new Promise((resolve, reject) => {
+      this.http.get(queryUrl).subscribe(
+        (data: any) => {
+          if (data['response']['docs'][0] != undefined) {
+            this.results = data['response']['docs'][0];
+          } else {
+              this.results = undefined;
+          }
+          resolve(this.results);
+        },
+        (error) => {
+          reject(error);
         }
-      },
-      error: (error) => {
-        console.log(error);
-      }
+      );
     });
   }
 
